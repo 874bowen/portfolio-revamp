d8c483d3300106d5e602682be1ab3d54c47184b8
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2023-11-14 08:49:32 +0300

    update theme context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import './App.css'
+import { useState } from "react";
+import { ThemeContext } from "./services/providers/ThemeContext";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
 
 function App() {
-  return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+	const [theme, setTheme] = useState("light");
+	const toggleTheme = () => {
+		if (theme === "light") {
+			setTheme("dark");
+		} else {
+			setTheme("light");
+		}
+	};
+
+	return (
+		<div className={theme == "light" ? "bg-[whitesmoke]" : "bg-[#242424]"}>
+			<ThemeContext.Provider value={theme}>
+				<Navbar toggleTheme={toggleTheme} />
+				<div className="pt-[70px]"></div>
+				<Home />
+				<About />
+				<Projects />
+			</ThemeContext.Provider>
+		</div>
+	);
 }
 
-export default App
+export default App;
