ca2cbc7b1fa4a748f7207780ce39008394c7ef91
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2023-11-14 08:48:19 +0300

    use classed components for a tag

---
+++ src/components/ui/classed-ui/Link.tsx
@@ -0,0 +1,30 @@
+import classed from "classed-components";
+
+const Link = classed.a([
+   // "rounded-full",
+   // "bg-[#0747E7]",
+   "px-4",
+   "py-3",
+   // "text-sm",
+   // "font-semibold",
+   // "text-white",
+   // "shadow-sm",
+   // "hover:bg-[#769CF4]",
+   // "focus-visible:outline",
+   // "focus-visible:outline-2",
+   // "focus-visible:outline-offset-2",
+   // "focus-visible:outline-indigo-600",
+   "hover:cursor-pointer",
+   "active:scale-95",
+   "active:text-[#0747E7]",
+   "active:border-b",
+   "active:border-b-2",
+   "active:border-b-[#0747E7]",  
+   "active:font-semibold",
+   // "dark and active"
+   "dark:hover:bg-[#242424]",
+   "dark:hover:text-[whitesmoke]",
+   "dark:active:border-b-[whitesmoke]",
+])
+
+export default Link;
