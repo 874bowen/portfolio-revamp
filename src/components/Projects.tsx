ff35fbbfc3c47db6925fd3eb967d7997359b5384
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2023-11-25 23:45:14 +0300

    update projects by adding hover contents

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -23,7 +23,42 @@ export default function Projects() {
                 <div className="flex-col md:w-4/12 md:pr-1">
                   <div className="relative">
                   <div className="relative block min-w-full group bg-gray-900 h-[500px] rounded-lg">
-                    <img src={MedImg} alt="" height="600" className="absolute w-full min-h-[500px] max-h-[500px] project-img object-cover pb-1 rounded-lg group-hover:opacity-30" style={theme == "dark" ? {filter: "grayscale(100%)"} : {}} />
+                    <img src={MedImg} alt="" height="600" className="absolute w-full min-h-[500px] max-h-[500px] project-img object-cover pb-1 rounded-lg group-hover:opacity-30" style={theme == "dark" ? {filter: "grayscale(100%)"} : {background: "linear-gradient(45deg, rgba(2,0,36,1) 0%, rgba(7,71,231,0.5) 35%, rgba(255,255,255,0.5) 100%)"}} />
+                    <div className="relative h-full flex justify-center items-center p-5">
+                        <div className="">
+                            {/* Hidden content */}
+                            <div
+                                className="transition-all transform 
+                                translate-y-8 opacity-0 
+                                group-hover:opacity-100 
+                                group-hover:translate-y-0"
+                            >
+                                <div className="p-2">
+                                    <p className="text-lg text-white font-semibold">
+                                        The Child Protection Management Information System 
+                                    </p>
+                                    <small className="text-white py-4">
+                                    Served as a team lead in a hackathon, where the challenge was to upgrade the Child Protection Information Management System (CPIMS) from Python 2.17 to 3.10 and Django 1.18 to Django 4.0, as well as generate unit tests. 
+                                    </small>
+                                    <button
+                                        className="px-4 py-2 mt-2 text-sm 
+                                            text-white bg-[#0747E7] flex items-center gap-2 rounded-sm"
+                                    >
+                                        View on Github 
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                          <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                            {/* End of hidden content */}
+                        </div>
+                    </div>
+                  </div>
+                  </div>
+                  <div className="">
+                  <div className="relative block group min-w-full group bg-gray-900 h-[300px] rounded-lg my-1">
+                    <img src={Rick} alt="" height="600" className="absolute w-full min-h-[300px] max-h-[300px] project-img object-cover pb-1 rounded-lg group-hover:opacity-30" style={theme == "dark" ? {filter: "grayscale(100%)"} : {}} />
                     <div className="relative h-full flex justify-center items-center p-5">
                         <div className="">
                             {/* Hidden content */}
@@ -35,8 +70,7 @@ export default function Projects() {
                             >
                                 <div className="p-2">
                                     <p className="text-lg text-white">
-                                        Welcome to
-                                        GeeksforGeeks.
+                                        
                                     </p>
                                     <button
                                         className="px-4 py-2 text-sm 
@@ -49,11 +83,6 @@ export default function Projects() {
                             {/* End of hidden content */}
                         </div>
                     </div>
-                  </div>
-                  </div>
-                  <div className="">
-                  <div className="relative block group">
-                    <img src={Rick} alt="" height="600" className="w-full min-h-[300px] max-h-[300px] project-img object-cover pt-1 rounded-lg" style={theme == "dark" ? {filter: "grayscale(100%)"} : {}} />
                   </div>
                   </div>
                 </div>
@@ -77,44 +106,6 @@ export default function Projects() {
               </div>
             </div>
 					</div>
-          <div className="flex items-center justify-center h-screen">
-      <a className="relative block w-1/4 bg-gray-900 group" href="#">
-        <img className="absolute inset-0 object-cover w-full h-full group-hover:opacity-50"
-          src="https://cdn.pixabay.com/photo/2021/10/30/08/07/desert-6753729__340.jpg" alt="" />
-        <div className="relative p-2">
-          <div className="mt-40">
-            <div
-              className="transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0">
-              <div className="p-2">
-                <p className="text-sm text-white">
-                  image hover in text using tailwind css
-                </p>
-                <button className="px-4 py-2 text-sm text-white bg-indigo-600">Read More</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </a>
-    </div>
-          <div className="flex items-center justify-center h-screen">
-      <a className="relative block w-1/4 bg-gray-900 group" href="#">
-        <img className="absolute inset-0 object-cover w-full h-full group-hover:opacity-50"
-          src="https://cdn.pixabay.com/photo/2021/10/30/08/07/desert-6753729__340.jpg" alt="" />
-        <div className="relative p-2">
-          <div className="mt-40">
-            <div
-              className="transition-all transform translate-y-8 opacity-0 group-hover:opacity-100 group-hover:translate-y-0">
-              <div className="p-2">
-                <p className="text-sm text-white">
-                  image hover in text using tailwind css
-                </p>
-                <button className="px-4 py-2 text-sm text-white bg-indigo-600">Read More</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </a>
-    </div>
 				</div>
 			)}
 		</ThemeContext.Consumer>
